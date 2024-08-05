f81a2dc605db660e2ee4480f09151ad885972f2b
amienramdhani <mar081201@gmail.com>
2024-08-05 19:52:58 +0700

    update API

--- src/customApi.js
+++ src/customApi.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const customApi = axios.create({
-  baseURL: 'https://laravel-production-9db3.up.railway.app/api/v1',
+  baseURL:
+    process.env.VUE_APP_API_BASE_URL ||
+    'https://laravel-production-9db3.up.railway.app/api/v1', // Ganti dengan URL API Anda
 });
 
 export default customApi;
